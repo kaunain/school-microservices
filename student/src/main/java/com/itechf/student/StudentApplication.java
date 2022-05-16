cc04a6eec43b9fe87e5f5629ab453d4d4acb96ff
Kaunain Ahmad <kaunain.ahmad2000@gmail.com>
2022-05-16 18:23:15 +0530

    RestTemplate code added

--- student/src/main/java/com/itechf/student/StudentApplication.java
+++ student/src/main/java/com/itechf/student/StudentApplication.java
@@ -2,7 +2,10 @@ package com.itechf.student;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.client.RestTemplateAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @EnableEurekaClient
 @SpringBootApplication
@@ -12,4 +15,14 @@ public class StudentApplication {
 		SpringApplication.run(StudentApplication.class, args);
 	}
 
+	@Bean
+	public Teacher getTeacher() {
+		Teacher classTeacher = new Teacher(1, "name", "ClassName");
+		return classTeacher;
+	}
+
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
 }
--- student/src/main/java/com/itechf/student/StudentController.java
+++ student/src/main/java/com/itechf/student/StudentController.java
@@ -3,19 +3,31 @@ package com.itechf.student;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class StudentController {
 
     @Autowired
     private StudentDao dao;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    // String teacherUri = "http://localhost:8082/teacher/1";
+    String teacherUri = "http://localhost:8082/teacher/1";
     
     @RequestMapping("/student/{id}")
     public Student getStudent(@PathVariable Integer id) {
-        return dao.getStudent(id);
+        Student stu = dao.getStudent(id);
+        ResponseEntity<Teacher> teacher = restTemplate.getForEntity(teacherUri, Teacher.class);
+        System.out.println("\n\n\n\n------------------" + teacher.getBody());
+        stu.setClassTeacher(teacher.getBody());
+        return stu;
     }
     @RequestMapping("/all")
     public List<Student> getStudents() {
